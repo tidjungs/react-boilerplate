4b3e6b9513d51d29ed7e5b117c24796f4e167d2d
tidjungs <waewprach.s@ku.th>
2016-09-06 23:27:49 +0700

    functional Component Page

--- ui/components/Pages.js
+++ ui/components/Pages.js
@@ -1,44 +1,38 @@
 import React, { Component, PropTypes } from 'react'
 import Page from './Page'
 
-export default class Pages extends Component {
+const Pages = ({ pages, onReloadPages }) => (
+	<div>
+		<button 
+        	className='button'
+        	onClick={() => onReloadPages()}>
+        	Reload Pages
+        </button>
+        <hr />
+      	<table className='table'>
+      		<thead>
+          		<tr>
+	            <th>ID</th>
+	            <th>Title</th>
+	            <th>Action</th>
+	          </tr>
+	        </thead>
+	        <tbody>
+	      		{ 
+	      			pages.map((page) => (
+	      				<Page title={ page.title } 
+							key={ page.id }
+							id={ page.id } />
+	      			)) 
+	      		}
+	        </tbody>
+	    </table>
+    </div>
+)
 
-	static propTypes = {
-    	pages: PropTypes.array.isRequired,
-    	onReloadPages: PropTypes.func.isRequired
-  	}
-
-	render() {
-		
-		const { pages, onReloadPages } = this.props
-
-		const rowElement = pages.map((page) => 
-			<Page title={ page.title } 
-				key={ page.id }
-				id={ page.id } />
-		)
-
-		return (
-			<div>
-				<button 
-		        	className='button'
-		        	onClick={() => onReloadPages()}>
-		        	Reload Pages
-		        </button>
-		        <hr />
-		      	<table className='table'>
-		      		<thead>
-		          		<tr>
-			            <th>ID</th>
-			            <th>Title</th>
-			            <th>Action</th>
-			          </tr>
-			        </thead>
-			        <tbody>
-			      		{ rowElement }
-			        </tbody>
-			     </table>
-		     </div>
-	    )
-	}
+Pages.propTypes = {
+	pages: PropTypes.array.isRequired,
+	onReloadPages: PropTypes.func.isRequired
 }
+
+export default Pages
