1a556a7590a96d193ea803a4c74c3fecb2d4ffe1
tidjungs <waewprach.s@ku.th>
2016-09-06 14:50:23 +0700

    add routes

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,5 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import routes from './routes'
 
-import {
-	App,
-	HelloWorld,
-	Page
-} from './components'
-
-render((
-	<Router history={ browserHistory }>
-		<Route path='/' component={ App }>
-      		<IndexRoute component={ HelloWorld } />
-      		<Route path='page' component={ Page } />
-    	</Route>
-	</Router>
-), document.getElementById('app'))
+render(routes(), document.getElementById('app'))
---
+++ ui/routes.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import {
+	App,
+	HelloWorld,
+	Page
+} from './components'
+
+export default () => {
+  return (
+    <Router history={ browserHistory }>
+      <Route path='/'
+             component={ App }>
+        <IndexRoute component={ HelloWorld } />
+        <route path='page'
+               component={ Page } />
+      </Route>
+    </Router>
+  )
+}
