8cb5aede693fa717cb1fe3a7089462c23e0ebfd4
tidjungs <waewprach.s@ku.th>
2016-09-07 22:45:53 +0700

    rm cd

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,11 @@ import { render } from 'react-dom'
 import routes from './routes'
 import './theme/element.scss'
 
+import { AppContainer } from 'react-hot-loader'
 
-render(routes(), document.getElementById('app'))
+const rootEl = document.getElementById('app')
+
+render(
+	routes(),
+	rootEl
+)
