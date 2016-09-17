be4be5d410501eae58f1edc7575b5af00badc051
tidjungs <waewprach.s@ku.th>
2016-09-17 20:00:20 +0700

    reformat action

--- ui/actions/page.js
+++ ui/actions/page.js
@@ -1,11 +1,21 @@
 import { CALL_API } from 'redux-api-middleware'
 import { PAGES_ENDPOINT } from '../constants/endpoints'
 
+import {
+  LOAD_PAGES_REQUEST,
+  LOAD_PAGES_SUCCESS,
+  LOAD_PAGES_FAILURE,
+
+  LOAD_PAGE_REQUEST,
+  LOAD_PAGE_SUCCESS,
+  LOAD_PAGE_FAILURE
+} from '../constants/actionTypes'
+
 export const loadPages = () => ({
   [CALL_API]: {
     endpoint: PAGES_ENDPOINT,
     method: 'GET',
-    types: ['LOAD_PAGES_REQUEST', 'LOAD_PAGES_SUCCESS', 'LOAD_PAGES_FAILURE']
+    types: [LOAD_PAGES_REQUEST, LOAD_PAGES_SUCCESS, LOAD_PAGES_FAILURE]
   }
 })
 
@@ -13,6 +23,6 @@ export const loadPage = (id) => ({
   [CALL_API]: {
     endpoint: `${PAGES_ENDPOINT}/${id}`,
     method: 'GET',
-    types: ['LOAD_PAGE_REQUEST', 'LOAD_PAGE_SUCCESS', 'LOAD_PAGE_FAILURE']
+    types: [LOAD_PAGE_REQUEST, LOAD_PAGE_SUCCESS, LOAD_PAGE_FAILURE]
   }
 })
--- ui/reducers/pages.js
+++ ui/reducers/pages.js
@@ -1,14 +1,19 @@
+import {
+ 	LOAD_PAGES_SUCCESS,
+ 	LOAD_PAGE_SUCCESS
+}	from '../constants/actionTypes'
+
 const initialState = []
 
 export default (state = initialState, action) => {
-  switch(action.type) {
-    case 'LOAD_PAGES_SUCCESS':
-      return action.payload
-    case 'LOAD_PAGE_SUCCESS':
-      return [action.payload]
-    default:
-      return state
-  }
+  	switch(action.type) {
+    	case LOAD_PAGES_SUCCESS:
+      		return action.payload
+    	case LOAD_PAGE_SUCCESS:
+      		return [action.payload]
+    	default:
+      		return state
+  	}
 }
 
 export const getPageById = (state, id) => (
