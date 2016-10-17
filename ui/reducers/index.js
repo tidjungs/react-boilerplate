aafc445e6c4063af4f377d81e690547f64c7bef6
tidjungs <waewprach.s@ku.th>
2016-10-17 18:52:06 +0700

    delete form

--- ui/reducers/index.js
+++ ui/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
 import pages from './pages'
-import { reducer as formReducer } from 'redux-form'
 
 
 export default combineReducers({
-	form: formReducer,
 	pages
 })
